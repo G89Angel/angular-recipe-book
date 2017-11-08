8cff602208f468941e20a22d0b488aeb411388d9
G89Angel <g89.angel@gmail.com>
2017-11-08 18:03:27 +0100

    Add ingredients behaviour

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,24 @@
-import { Ingredient } from './../shared/ingredient.model';
-import { Component, OnInit } from '@angular/core';
+import {Ingredient} from './../shared/ingredient.model';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 3),
-    new Ingredient('Tomatos', 10)
-  ];
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 3),
+        new Ingredient('Tomatos', 10)
+    ];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onIngredientAdded(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
 }
