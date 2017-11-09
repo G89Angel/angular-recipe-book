d64ab915ddda2d5c402123c679d42c974b117910
G89Angel <g89.angel@gmail.com>
2017-11-09 22:54:29 +0100

    Send ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,19 +1,22 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
-import {RecipeService} from "../recipe.service";
+import {RecipeService} from '../recipe.service';
 
 @Component({
-    selector: 'app-recipe-detail',
-    templateUrl: './recipe-detail.component.html',
-    styleUrls: ['./recipe-detail.component.css']
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-    @Input() recipe: Recipe;
+  @Input() recipe: Recipe;
 
-    constructor(private recipeService: RecipeService) {
-    }
+  constructor(private recipeService: RecipeService) {
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,14 @@
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
-import {Ingredient} from "../shared/ingredient.model";
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService) {
+  }
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -11,16 +16,16 @@ export class RecipeService {
         'This is just a german recipe',
         'https://c.pxhere.com/photos/ba/e4/sideburn_pig_baked_potato_salad_potatoes_green_salad_eat_food-895723.jpg!d',
         [
-            new Ingredient('Meat', 1),
-            new Ingredient('French Fries', 20)
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
         ]),
     new Recipe(
         'Big Fat Burger',
         'What else you need to say?',
-        'https://cdn.pixabay.com/photo/2017/08/08/18/09/burger-2612137_960_720.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/d/dc/Lounge_Burger_Wiki.jpg',
         [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1)
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1)
         ])
   ];
 
@@ -29,5 +34,7 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
