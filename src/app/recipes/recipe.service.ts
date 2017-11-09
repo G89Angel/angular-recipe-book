80e788b21f5934a439a65ef1badc0503aba120ed
G89Angel <g89.angel@gmail.com>
2017-11-09 22:35:02 +0100

    List ingredients per product

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Dummy recipe', 'This is just a dummy recipe', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Chicken-Avocado-Heart-Salad-1735138.jpg'),
-    new Recipe('Another Dummy recipe', 'This is just a dummy recipe', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Chicken-Avocado-Heart-Salad-1735138.jpg')
+    new Recipe(
+        'Schnitzel',
+        'This is just a german recipe',
+        'https://c.pxhere.com/photos/ba/e4/sideburn_pig_baked_potato_salad_potatoes_green_salad_eat_food-895723.jpg!d',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]),
+    new Recipe(
+        'Big Fat Burger',
+        'What else you need to say?',
+        'https://cdn.pixabay.com/photo/2017/08/08/18/09/burger-2612137_960_720.jpg',
+        [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+        ])
   ];
 
   getRecipes() {
