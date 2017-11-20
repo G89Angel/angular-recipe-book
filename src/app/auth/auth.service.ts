04c9dcea60d68ba229d18032f5d30a0f8cbc7cf8
G89Angel <g89.angel@gmail.com>
2017-11-20 18:22:56 +0100

    Supply token in storage service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .catch(
@@ -11,10 +13,21 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then((response) => {
-          console.log(response);
+          this.getToken();
         })
         .catch((response) => {
           console.log(response);
         });
+  }
+
+  getToken() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        user.getIdToken().then((data) => {
+          this.token = data;
+        });
+      }
+    });
+    return this.token;
   }
 }
